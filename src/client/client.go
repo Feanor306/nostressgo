b1453664653ca6b8136e757fa1559a08ab313fe0
George Parsehyan <feanor306@live.com>
2024-03-08 13:55:29 +0100

    refactored client, added subMutex

--- src/client/client.go
+++ src/client/client.go
@@ -3,6 +3,8 @@ package client
 import (
 	"fmt"
 	"log"
+	"sync"
+	"time"
 
 	"github.com/feanor306/nostressgo/src/service"
 	"github.com/feanor306/nostressgo/src/types"
@@ -11,10 +13,12 @@ import (
 )
 
 type Client struct {
-	PubKey        string
-	Hub           Hub
-	Conn          *websocket.Conn
-	Service       *service.Service
+	PubKey  string
+	Hub     Hub
+	Conn    *websocket.Conn
+	Service *service.Service
+
+	SubMutex      sync.RWMutex
 	Subscriptions map[string]*nostr.ReqEnvelope
 }
 
@@ -26,6 +30,7 @@ func NewClient(conn *websocket.Conn, svc *service.Service, hub *Hub) *Client {
 	}
 }
 
+// Read handles client websocket read loop
 func (c *Client) Read() {
 	defer func() {
 		c.Service.DB.Close()
@@ -57,6 +62,115 @@ func (c *Client) Read() {
 	}
 }
 
+func (c *Client) HandleRequestMessage(ew *types.EnvelopeWrapper, chanGroup *types.ChanGroup) {
+	switch ew.Envelope.Label() {
+	case types.EVENT:
+		go c.HandleEvent(ew, chanGroup)
+	case types.REQ:
+		go c.HandleRequestSubscription(ew, chanGroup)
+	case types.CLOSE:
+		go c.HandleCloseSubscription(ew, chanGroup)
+	default:
+		chanGroup.Chan <- ew.NoticeResponse(fmt.Errorf("invalid payload"))
+		defer chanGroup.Done()
+		return
+	}
+}
+
+func (c *Client) HandleEvent(ew *types.EnvelopeWrapper, chanGroup *types.ChanGroup) {
+	defer chanGroup.Done()
+	var err error
+	ee, ok := ew.Envelope.(*nostr.EventEnvelope)
+
+	if !ok {
+		chanGroup.Chan <- ew.EventResponse(fmt.Errorf("unable to parse event"))
+		return
+	}
+
+	event := types.NewEvent(&ee.Event)
+
+	if err := event.Validate(); err != nil {
+		chanGroup.Chan <- ew.EventResponse(err)
+		return
+	}
+
+	if len(c.PubKey) > 0 {
+		c.PubKey = event.PubKey
+	}
+
+	switch ee.Event.Kind {
+	case 0:
+		err = c.Service.HandleZeroEvent(&ee.Event)
+	case 1:
+		err = c.Service.CreateEvent(&ee.Event)
+	}
+
+	chanGroup.Chan <- ew.EventResponse(err)
+	c.Hub.Broadcast <- &ee.Event
+}
+
+func (c *Client) HandleRequestSubscription(ew *types.EnvelopeWrapper, chanGroup *types.ChanGroup) {
+	defer chanGroup.Done()
+	reqEnv, ok := ew.Envelope.(*nostr.ReqEnvelope)
+
+	if !ok {
+		chanGroup.Chan <- ew.NoticeResponse(fmt.Errorf("unable to parse req"))
+		return
+	}
+
+	c.SubMutex.Lock()
+	defer c.SubMutex.Unlock()
+	if _, ok := c.Subscriptions[reqEnv.SubscriptionID]; ok {
+		chanGroup.Chan <- ew.ClosedResponse(reqEnv.SubscriptionID, "subscription already exists")
+		return
+	}
+
+	c.Subscriptions[reqEnv.SubscriptionID] = reqEnv
+
+	for _, filter := range reqEnv.Filters {
+		cg := types.NewChanGroup()
+		go func() {
+			cg.WaitClose()
+		}()
+		if err := c.Service.DB.GetEventsByFilter(&filter, cg); err != nil {
+			chanGroup.Chan <- ew.ClosedResponse(reqEnv.SubscriptionID, err.Error())
+			return
+		}
+
+		count := 0
+		for event := range cg.Chan {
+			count++
+			if filter.Limit > 0 && count >= filter.Limit {
+				// pause sending because of limit
+				// all events matching subscription should be sent eventually
+				time.Sleep(time.Second * 10)
+			}
+			chanGroup.Chan <- event.EventResponse(nil)
+		}
+		chanGroup.Chan <- ew.EoseResponse()
+	}
+}
+
+func (c *Client) HandleCloseSubscription(ew *types.EnvelopeWrapper, chanGroup *types.ChanGroup) {
+	defer chanGroup.Done()
+	closeEnv, ok := ew.Envelope.(*nostr.CloseEnvelope)
+
+	if !ok {
+		chanGroup.Chan <- ew.NoticeResponse(fmt.Errorf("unable to parse close"))
+		return
+	}
+
+	c.SubMutex.Lock()
+	defer c.SubMutex.Unlock()
+	if _, ok := c.Subscriptions[closeEnv.String()]; ok {
+		delete(c.Subscriptions, closeEnv.String())
+		chanGroup.Chan <- ew.ClosedResponse(closeEnv.String(), "subscription removed successfully")
+		return
+	}
+
+	chanGroup.Chan <- ew.NoticeResponse(fmt.Errorf("subscription not found"))
+}
+
 func (c *Client) Respond(result *types.EnvelopeWrapper) {
 	data, err := result.MarshalJSON()
 	if err != nil {
@@ -81,106 +195,5 @@ func (c *Client) SendErrorResponse(err error) {
 	err = c.Conn.WriteMessage(1, data)
 	if err != nil {
 		log.Println(err)
-	}
-}
-
-func (c *Client) HandleNewSubscription(ew *types.EnvelopeWrapper, chanGroup *types.ChanGroup) {
-	reqEnv, ok := ew.Envelope.(*nostr.ReqEnvelope)
-
-	if !ok {
-		chanGroup.Chan <- ew.NoticeResponse(fmt.Errorf("unable to parse req"))
-		return
-	}
-
-	if _, ok := c.Subscriptions[reqEnv.SubscriptionID]; ok {
-		chanGroup.Chan <- ew.ClosedResponse(reqEnv.SubscriptionID, "subscription already exists")
-	}
-
-	c.Subscriptions[reqEnv.SubscriptionID] = reqEnv
-
-	for _, filter := range reqEnv.Filters {
-		cg := types.NewChanGroup()
-		go func() {
-			cg.WaitClose()
-		}()
-		if err := c.Service.DB.GetEventsByFilter(&filter, cg); err != nil {
-			chanGroup.Chan <- ew.ClosedResponse(reqEnv.SubscriptionID, err.Error())
-			return
-		}
-
-		count := 0
-		for event := range cg.Chan {
-			count++
-			if filter.Limit > 0 && count >= filter.Limit {
-				// return or wait a bit and send the rest?
-				return
-			}
-			chanGroup.Chan <- event.EventResponse(nil)
-		}
-		chanGroup.Chan <- ew.EoseResponse()
-	}
-}
-
-func (c *Client) RemoveSubscription(subscriptionId string) error {
-	if _, ok := c.Subscriptions[subscriptionId]; ok {
-		delete(c.Subscriptions, subscriptionId)
-		return nil
-	}
-
-	return fmt.Errorf("subscription not found")
-}
-
-func (c *Client) HandleRequestMessage(ew *types.EnvelopeWrapper, chanGroup *types.ChanGroup) {
-	defer chanGroup.Done()
-
-	// CLIENT => SERVER message handling
-	switch ew.Envelope.Label() {
-	case types.EVENT:
-		var err error
-		ee, ok := ew.Envelope.(*nostr.EventEnvelope)
-
-		if !ok {
-			chanGroup.Chan <- ew.EventResponse(fmt.Errorf("unable to parse event"))
-			return
-		}
-
-		event := types.NewEvent(&ee.Event)
-
-		if err := event.Validate(); err != nil {
-			chanGroup.Chan <- ew.EventResponse(err)
-			return
-		}
-
-		if len(c.PubKey) > 0 {
-			c.PubKey = event.PubKey
-		}
-
-		switch ee.Event.Kind {
-		case 0:
-			err = c.Service.HandleZeroEvent(&ee.Event)
-		case 1:
-			err = c.Service.CreateEvent(&ee.Event)
-		}
-
-		chanGroup.Chan <- ew.EventResponse(err)
-		// Notify all other clients, use hub?
-	case types.REQ:
-		// defer of chanGroup.Done will close the channel we send prematurely, find a fix?
-		go c.HandleNewSubscription(ew, chanGroup)
-		return
-	case types.CLOSE:
-		closeEnv, ok := ew.Envelope.(*nostr.CloseEnvelope)
-
-		if !ok {
-			chanGroup.Chan <- ew.NoticeResponse(fmt.Errorf("unable to parse close"))
-			return
-		}
-
-		c.RemoveSubscription(closeEnv.String())
-		chanGroup.Chan <- ew.ClosedResponse(closeEnv.String(), "subscription removed successfully")
-		return
-	default:
-		chanGroup.Chan <- ew.NoticeResponse(fmt.Errorf("invalid payload"))
-		return
 	}
 }
