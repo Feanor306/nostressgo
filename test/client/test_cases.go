f9b9635bdad79cfcbea8889141a6ba1f1e9d2ba7
George Parsehyan <feanor306@live.com>
2024-03-11 21:08:45 +0100

    added test cases

--- test/client/test_cases.go
+++ test/client/test_cases.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"time"
+
 	"github.com/nbd-wtf/go-nostr"
 )
 
@@ -22,9 +24,17 @@ func (tc *TestCase) SerializeResponse() ([]byte, error) {
 
 func GetTestCases(publicKey, privateKey string) []TestCase {
 	cases := make([]TestCase, 0, 100)
+	cases = append(cases, GetEvent1TestCases(publicKey, privateKey)...)
+	cases = append(cases, GetEvent0TestCases(publicKey, privateKey)...)
+
+	return cases
+}
+
+func GetEvent1TestCases(publicKey, privateKey string) []TestCase {
+	cases := make([]TestCase, 0, 10)
 
 	// Valid event kind 1
-	createEvent := &nostr.Event{
+	event1 := &nostr.Event{
 		PubKey:    publicKey,
 		CreatedAt: nostr.Now(),
 		Kind:      nostr.KindTextNote,
@@ -33,20 +43,123 @@ func GetTestCases(publicKey, privateKey string) []TestCase {
 			{"p", P_TAG}},
 		Content: "Hello Worlddasdsdf!",
 	}
-	createEvent.Sign(privateKey)
-
+	event1.Sign(privateKey)
 	createValid1 := TestCase{
 		Request: &nostr.EventEnvelope{
-			Event: *createEvent,
+			Event: *event1,
 		},
 		Response: &nostr.OKEnvelope{
-			EventID: createEvent.ID,
+			EventID: event1.ID,
 			OK:      true,
 			Reason:  "event saved successfully",
 		},
 	}
-
 	cases = append(cases, createValid1)
+
+	// invalid pubkey
+	event1Invalid1 := &nostr.Event{
+		PubKey:    publicKey + "a",
+		CreatedAt: nostr.Now(),
+		Kind:      nostr.KindTextNote,
+		Tags: nostr.Tags{
+			{"e", E_TAG, "wss://nostr.example.com"},
+			{"p", P_TAG}},
+		Content: "Hello Worlddasdsdf!",
+	}
+	event1Invalid1.Sign(privateKey)
+	createInvalid1 := TestCase{
+		Request: &nostr.EventEnvelope{
+			Event: *event1Invalid1,
+		},
+		Response: &nostr.OKEnvelope{
+			EventID: event1Invalid1.ID,
+			OK:      false,
+			Reason:  "invalid pubkey",
+		},
+	}
+	cases = append(cases, createInvalid1)
+
+	// invalid date
+	event1Invalid2 := &nostr.Event{
+		PubKey:    publicKey,
+		CreatedAt: nostr.Timestamp(time.Now().Add(time.Hour).Unix()),
+		Kind:      nostr.KindTextNote,
+		Tags: nostr.Tags{
+			{"e", E_TAG, "wss://nostr.example.com"},
+			{"p", P_TAG}},
+		Content: "Hello Worlddasdsdf!",
+	}
+	event1Invalid2.Sign(privateKey)
+	createInvalid2 := TestCase{
+		Request: &nostr.EventEnvelope{
+			Event: *event1Invalid2,
+		},
+		Response: &nostr.OKEnvelope{
+			EventID: event1Invalid2.ID,
+			OK:      false,
+			Reason:  "invalid pubkey",
+		},
+	}
+	cases = append(cases, createInvalid2)
+	return cases
+}
+
+func GetEvent0TestCases(publicKey, privateKey string) []TestCase {
+	cases := make([]TestCase, 0, 10)
+
+	// Valid event kind 0
+	event0valid1 := &nostr.Event{
+		PubKey:  publicKey,
+		Content: "{\"name\":\"Bob\", \"about\":\"normal dude\", \"picture\":\"face.jpg\"}",
+	}
+	event0valid1.Sign(privateKey)
+	create0Valid1 := TestCase{
+		Request: &nostr.EventEnvelope{
+			Event: *event0valid1,
+		},
+		Response: &nostr.OKEnvelope{
+			EventID: event0valid1.ID,
+			OK:      true,
+			Reason:  "event saved successfully",
+		},
+	}
+	cases = append(cases, create0Valid1)
+
+	// Valid event kind 0 should replace previous
+	event0valid2 := &nostr.Event{
+		PubKey:  publicKey,
+		Content: "{\"name\":\"Robert\", \"about\":\"normal friend\", \"picture\":\"head.jpg\"}",
+	}
+	event0valid2.Sign(privateKey)
+	create0Valid2 := TestCase{
+		Request: &nostr.EventEnvelope{
+			Event: *event0valid2,
+		},
+		Response: &nostr.OKEnvelope{
+			EventID: event0valid2.ID,
+			OK:      true,
+			Reason:  "event saved successfully",
+		},
+	}
+	cases = append(cases, create0Valid2)
+
+	// Invalid event kind 0
+	event0invalid1 := &nostr.Event{
+		PubKey:  publicKey,
+		Content: "{\"name\":\"Bob\", \"about\":\"normal dude\", \"picture\":\"face.jpg\"}}",
+	}
+	event0invalid1.Sign(privateKey)
+	create0Invalid1 := TestCase{
+		Request: &nostr.EventEnvelope{
+			Event: *event0invalid1,
+		},
+		Response: &nostr.OKEnvelope{
+			EventID: event0invalid1.ID,
+			OK:      false,
+			Reason:  "invalid content",
+		},
+	}
+	cases = append(cases, create0Invalid1)
 
 	return cases
 }
